77fa3f81123b5d35a115b96e8eb5f5f3059191a6
Akshat Upadhyay <48818359+AkshatUpadhyay@users.noreply.github.com>
2020-07-25 21:43:53 +0530

    Main Code

---
+++ Caeser Cipher Through Brute force.py
@@ -0,0 +1,19 @@
+# Caesar Cipher Decoder
+
+print('The program was coded by Akshat Upadhyay\n\n\n')
+
+
+message = 'GUVF VF ZL FRPERG ZRFFNTR.'
+LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+for key in range(len(LETTERS)):
+    translated = ''
+    for symbol in message:
+        if symbol in LETTERS:
+            num = LETTERS.find(symbol) 
+            num = num - key
+            if num < 0:
+                num = num + len(LETTERS)
+            translated = translated + LETTERS[num]
+        else:
+            translated = translated + symbol
+    print('%s : %s key' % (translated,key))
